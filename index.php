715852bf3e093e6fc4d119570f4a8dfc9499c564
tobiasnyman <tobias.nyman@optistud.fi>
2016-01-14 12:00:29 +0200

    update index,css.

--- index.php
+++ index.php
@@ -8,17 +8,42 @@
 <link rel="stylesheet" href="css/index.css" />
 </head>
 <body>
+<html class="no-js" lang="en">
+<head>
+<meta charset=utf-8>
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>indexgrej</title>
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
 
-<aside>
-Grejjer
-Grejjer
-Grejjer
-Grejjer
-Grejjer
-Grejjer
+<nav class="top-bar">
+		
+			<div class="top-bar-left">
+				<h4>Dashboard</h4>				
+			</div>
+				
+				<div class="top-bar-right">
+					<ul>
+						<li><a href="login.php" class="button">Logout</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		
+		
+		<aside class="show-for-medium">
+			<ul>
+				<a href="index.php?page=home"><li>Home</li></a>
+				<a href="index.php?page=about"><li>About</li></a>
+				<a href="index.php?page=options"><li>Options</li></a>
+			</ul>
+		</aside>
 
-</aside>
 
-
+</body>
+</html>
 </body>
 </html>
